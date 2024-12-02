76bcb5b1047896c4f561b5cfc81af38239144813
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-02 19:53:18 +0000

    split input into a module

---
+++ input.cppm
@@ -0,0 +1,50 @@
+export module input;
+import casein;
+import dotz;
+
+enum in_button {
+  R_LEFT = 0,
+  R_RIGHT,
+  S_LEFT,
+  S_RIGHT,
+  W_FRONT,
+  W_BACK,
+  IN_BTN_MAX,
+};
+static bool in_state[IN_BTN_MAX] {};
+static auto in_down(in_button b) { return [=] { in_state[b] = true; }; }
+static auto in_up(in_button b) { return [=] { in_state[b] = false; }; }
+
+static void in_setup_btn(casein::keys k, in_button b) {
+  using namespace casein;
+  handle(KEY_DOWN, k, in_down(b));
+  handle(KEY_UP,   k, in_up  (b));
+}
+
+static inline int in_axis(in_button n, in_button p) {
+  if (in_state[n] && !in_state[p]) return -1;
+  if (!in_state[n] && in_state[p]) return 1;
+  return 0;
+}
+static int in_r_axis() { return in_axis(R_LEFT, R_RIGHT); }
+static int in_s_axis() { return in_axis(S_LEFT, S_RIGHT); }
+static int in_w_axis() { return in_axis(W_BACK, W_FRONT); }
+
+export void in_update_camera(dotz::vec4 & c, float ms) {
+  auto a = c.w += in_r_axis() * ms / 1000.0f;
+  auto s = in_s_axis() * ms / 1000.0f;
+  auto w = in_w_axis() * ms / 1000.0f;
+  c.x += s * dotz::sin(a) + w * dotz::cos(a);
+  c.z += s * dotz::cos(a) - w * dotz::sin(a);
+}
+
+export void in_setup() {
+  using namespace casein;
+  in_setup_btn(K_LEFT,  R_LEFT);
+  in_setup_btn(K_RIGHT, R_RIGHT);
+
+  in_setup_btn(K_A, S_LEFT);
+  in_setup_btn(K_D, S_RIGHT);
+  in_setup_btn(K_W, W_FRONT);
+  in_setup_btn(K_S, W_BACK);
+}
--- poc.cpp
+++ poc.cpp
@@ -4,6 +4,7 @@
 
 import casein;
 import dotz;
+import input;
 import sitime;
 import vee;
 import voo;
@@ -14,52 +15,13 @@ struct upc {
   float time;
 } g_pc;
 
-enum in_button {
-  R_LEFT = 0,
-  R_RIGHT,
-  S_LEFT,
-  S_RIGHT,
-  W_FRONT,
-  W_BACK,
-  IN_BTN_MAX,
-};
-static bool in_state[IN_BTN_MAX] {};
-static auto in_down(in_button b) { return [=] { in_state[b] = true; }; }
-static auto in_up(in_button b) { return [=] { in_state[b] = false; }; }
-
-static void in_setup_btn(casein::keys k, in_button b) {
-  using namespace casein;
-  handle(KEY_DOWN, k, in_down(b));
-  handle(KEY_UP,   k, in_up  (b));
-}
-
-static inline int in_axis(in_button n, in_button p) {
-  if (in_state[n] && !in_state[p]) return -1;
-  if (!in_state[n] && in_state[p]) return 1;
-  return 0;
-}
-static int in_r_axis() { return in_axis(R_LEFT, R_RIGHT); }
-static int in_s_axis() { return in_axis(S_LEFT, S_RIGHT); }
-static int in_w_axis() { return in_axis(W_BACK, W_FRONT); }
-
 static void update_camera(float ms) {
-  auto a = g_pc.camera.w += in_r_axis() * ms / 1000.0f;
-  auto s = in_s_axis() * ms / 1000.0f;
-  auto w = in_w_axis() * ms / 1000.0f;
-  g_pc.camera.x += s * dotz::sin(a) + w * dotz::cos(a);
-  g_pc.camera.z += s * dotz::cos(a) - w * dotz::sin(a);
+  in_update_camera(g_pc.camera, ms);
 }
 
 struct thread : voo::casein_thread {
   thread() : casein_thread {} {
-    using namespace casein;
-    in_setup_btn(K_LEFT,  R_LEFT);
-    in_setup_btn(K_RIGHT, R_RIGHT);
-
-    in_setup_btn(K_A, S_LEFT);
-    in_setup_btn(K_D, S_RIGHT);
-    in_setup_btn(K_W, W_FRONT);
-    in_setup_btn(K_S, W_BACK);
+    in_setup();
   }
   void run() override {
     sitime::stopwatch t {};
